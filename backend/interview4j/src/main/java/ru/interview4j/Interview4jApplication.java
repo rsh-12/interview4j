b501ecf432400445d4fe8176fb1dcefc7e0dda1f
rsh-12 <grinvichforum12@mail.ru>
2021-07-16 21:19:45 +0500

    Add backend project

---
+++ backend/interview4j/src/main/java/ru/interview4j/Interview4jApplication.java
@@ -0,0 +1,13 @@
+package ru.interview4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Interview4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Interview4jApplication.class, args);
+    }
+
+}
---
+++ backend/interview4j/src/test/java/ru/interview4j/Interview4jApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.interview4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Interview4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
