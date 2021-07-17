fd9b3f124ebd1ffa632cc252c1f97e4281291377
rsh-12 <grinvichforum12@mail.ru>
2021-07-17 09:23:17 +0500

    Add UserTest class

---
+++ backend/interview4j/src/test/java/ru/interview4j/domain/UserTest.java
@@ -0,0 +1,58 @@
+package ru.interview4j.domain;
+/*
+ * Date: 17.07.2021
+ * Time: 8:33 AM
+ * */
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTest {
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setId("id");
+        user.setUsername();
+        user.setPassword();
+    }
+
+    @Test
+    public void getId_ShouldReturnId() {
+        assertEquals("id", user.getId());
+    }
+
+    @Test
+    public void getUsername_ShouldReturnUsername() {
+        assertEquals("username", user.getUsername());
+    }
+
+    @Test
+    public void getPassword_ShouldReturnPassword() {
+        assertEquals("password", user.getPassword());
+    }
+
+    @Test
+    public void getCreatedAt_ShouldReturnCreatedAt() {
+        assertEquals(new Date(), user.getCreatedAt());
+    }
+
+    @Test
+    public void getUpdatedAt_ShouldReturnUpdatedAt() {
+        assertEquals(new Date(), user.getUpdatedAt());
+    }
+
+    @Test
+    public void toString_ShouldBeEquals() {
+        String toStr = String.format("User{id='%s', username='%s', password='%s', createdAt='%s', updatedAt='%s'}",
+                user.getId(), user.getUsername(), user.getPassword(), user.getCreatedAt(), user.getUpdatedAt());
+        assertEquals(toStr, user.toString());
+    }
+
+}
