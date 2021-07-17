abed86e096a3f7958480f86c0ae1cf27c22616ed
rsh-12 <grinvichforum12@mail.ru>
2021-07-17 08:56:14 +0500

    Create stub User

---
+++ backend/interview4j/src/main/java/ru/interview4j/domain/User.java
@@ -0,0 +1,44 @@
+package ru.interview4j.domain;
+/*
+ * Date: 17.07.2021
+ * Time: 8:34 AM
+ * */
+
+import java.util.Date;
+
+public class User {
+
+    public void setId(String id) {
+
+    }
+
+    public void setUsername() {
+
+    }
+
+    public void setPassword() {
+    }
+
+    public String getUsername() {
+        return null;
+    }
+
+    public String getId() {
+        return null;
+    }
+
+    public Date getCreatedAt() {
+        return null;
+    }
+
+
+    public Date getUpdatedAt() {
+        return null;
+    }
+
+    public String getPassword() {
+        return null;
+    }
+
+
+}
