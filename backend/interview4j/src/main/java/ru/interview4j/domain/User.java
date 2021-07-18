c8d6af301aa19c7d9adfaf6fec06491005f9cb0b
rsh-12 <grinvichforum12@mail.ru>
2021-07-18 22:00:07 +0500

    Update User class

--- backend/interview4j/src/main/java/ru/interview4j/domain/User.java
+++ backend/interview4j/src/main/java/ru/interview4j/domain/User.java
@@ -5,40 +5,80 @@ package ru.interview4j.domain;
  * */
 
 import java.util.Date;
+import java.util.Objects;
 
 public class User {
 
+    private String id;
+    private String username;
+    private String password;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public String getId() {
+        return id;
+    }
+
     public void setId(String id) {
-
-    }
-
-    public void setUsername(String username) {
-
-    }
-
-    public void setPassword(String password) {
+        this.id = id;
     }
 
     public String getUsername() {
-        return null;
+        return username;
     }
 
-    public String getId() {
-        return null;
-    }
-
-    public Date getCreatedAt() {
-        return null;
-    }
-
-
-    public Date getUpdatedAt() {
-        return null;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
 
 }
