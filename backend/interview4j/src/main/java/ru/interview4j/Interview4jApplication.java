6f4368212cfa638021815d6932a3ba4466633806
rsh-12 <grinvichforum12@mail.ru>
2021-07-20 15:08:35 +0500

    Add EnableReactiveMongoAuditing annotation

--- backend/interview4j/src/main/java/ru/interview4j/Interview4jApplication.java
+++ backend/interview4j/src/main/java/ru/interview4j/Interview4jApplication.java
@@ -2,7 +2,9 @@ package ru.interview4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 
+@EnableReactiveMongoAuditing
 @SpringBootApplication
 public class Interview4jApplication {
 
