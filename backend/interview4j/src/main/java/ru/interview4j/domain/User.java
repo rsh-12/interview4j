bd58321674381c9032f4c4265134487ac41ec169
rsh-12 <grinvichforum12@mail.ru>
2021-07-22 10:19:45 +0500

    Add lombok support, update id

--- backend/interview4j/src/main/java/ru/interview4j/domain/User.java
+++ backend/interview4j/src/main/java/ru/interview4j/domain/User.java
@@ -5,16 +5,20 @@ package ru.interview4j.domain;
  * */
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.relational.core.mapping.Table;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Date;
-import java.util.Objects;
+
 
 /**
  * The main entity of the app, used for authentication/authorization,
@@ -22,18 +26,23 @@ import java.util.Objects;
  *
  * @author rsh-12
  */
-@Document(collection = "users")
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@EqualsAndHashCode(of = {"id", "username"})
+@Table("usr")
 public class User {
 
     @Id
-    private String id;
+    private Long id;
 
+    @NonNull
     @NotBlank
     @Size(min = 8, max = 16)
-    @Indexed(unique = true)
     private String username;
 
     @JsonIgnore
+    @NonNull
     @NotBlank
     @Size(min = 8, max = 128)
     private String password;
@@ -43,77 +52,5 @@ public class User {
 
     @LastModifiedDate
     private Date updatedAt;
-
-    public User() {
-    }
-
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        User user = (User) o;
-
-        if (!Objects.equals(id, user.id)) return false;
-        return username.equals(user.username);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + username.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id='" + id + '\'' +
-                ", username='" + username + '\'' +
-                ", createdAt=" + createdAt +
-                ", updatedAt=" + updatedAt +
-                '}';
-    }
 
 }
