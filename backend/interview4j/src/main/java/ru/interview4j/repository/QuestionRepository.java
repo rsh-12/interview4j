5318ee2a24a1ef92bc852a01be6dfe17fe9d760d
Ivanshelemeh <neo041990@mail.ru>
2021-07-22 18:11:32 +0300

    Questinrepository

---
+++ backend/interview4j/src/main/java/ru/interview4j/repository/QuestionRepository.java
@@ -0,0 +1,7 @@
+package ru.interview4j.repository;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import ru.interview4j.domain.Question;
+
+public interface QuestionRepository extends ReactiveCrudRepository<Question,Long> {
+}
