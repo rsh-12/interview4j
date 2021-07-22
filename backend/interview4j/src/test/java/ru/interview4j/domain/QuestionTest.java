dc2d2b259209f0b073271e02f6038ce1ac09c8b1
rsh-12 <grinvichforum12@mail.ru>
2021-07-22 21:57:15 +0500

    Update and add unit tests

---
+++ backend/interview4j/src/test/java/ru/interview4j/domain/QuestionTest.java
@@ -0,0 +1,82 @@
+package ru.interview4j.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class QuestionTest {
+
+    private Question question;
+
+    @BeforeEach
+    void setUp() {
+        question = new Question();
+        question.setTitle("New question");
+        question.setBody("Answer of question is");
+        question.setCreatedAt(new Date());
+        question.setUpdatedAt(new Date());
+    }
+
+    @Test
+    void getTitle_ShouldBeEqual() {
+        assertEquals("New question", question.getTitle());
+    }
+
+    @Test
+    void getBody_ShouldBeEqual() {
+        assertEquals("Answer of question is", question.getBody());
+    }
+
+    @Test
+    public void getCreatedAt_ShouldReturnCreatedAt() {
+        assertEquals(new Date().toString(), question.getCreatedAt().toString());
+    }
+
+    @Test
+    public void getUpdatedAt_ShouldReturnUpdatedAt() {
+        assertEquals(new Date().toString(), question.getUpdatedAt().toString());
+    }
+
+    @Test
+    public void equals_ShouldBeEqual() {
+        assertEquals(question, new Question("New question", "Body"));
+    }
+
+    @Test
+    public void equals_ShouldNotBeEqual() {
+        assertNotEquals(question, new Question("Title", "body"));
+    }
+
+    @Test
+    public void equals_SameObjects_ShouldBeEqual() {
+        assertEquals(question, question);
+    }
+
+    @Test
+    public void equals_Null_ShouldNotBeEqual() {
+        assertFalse(question.equals(null));
+    }
+
+    @Test
+    public void hashcode_Symmetric_ShouldBeEqual() {
+        Question newQuestion = new Question("New question", "Body");
+        assertTrue(question.equals(newQuestion) && newQuestion.equals(question));
+        assertEquals(question.hashCode(), newQuestion.hashCode());
+    }
+
+    @Test
+    public void toString_ShouldBeEqual() {
+        String toStr = String.format("Question(id=%s, title=%s, body=%s, createdAt=%s, updatedAt=%s)",
+                question.getId(), question.getTitle(), question.getBody(),
+                question.getCreatedAt(), question.getUpdatedAt());
+
+        assertEquals(toStr, question.toString());
+    }
+
+}
+
+
