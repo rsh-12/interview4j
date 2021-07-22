375b2345463f162ca17a14da8c81807248f5db53
rsh-12 <grinvichforum12@mail.ru>
2021-07-22 21:30:38 +0500

    Apply autoformatting

--- backend/interview4j/src/main/java/ru/interview4j/repository/QuestionRepository.java
+++ backend/interview4j/src/main/java/ru/interview4j/repository/QuestionRepository.java
@@ -3,5 +3,6 @@ package ru.interview4j.repository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import ru.interview4j.domain.Question;
 
-public interface QuestionRepository extends ReactiveCrudRepository<Question,Long> {
+public interface QuestionRepository extends ReactiveCrudRepository<Question, Long> {
+
 }
