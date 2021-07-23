21464758357b21f0459cf17a559aa9cecccf04f1
rsh-12 <grinvichforum12@mail.ru>
2021-07-23 08:43:17 +0500

    Update getter tests, constructor

--- backend/interview4j/src/test/java/ru/interview4j/domain/QuestionTest.java
+++ backend/interview4j/src/test/java/ru/interview4j/domain/QuestionTest.java
@@ -22,33 +22,21 @@ class QuestionTest {
     }
 
     @Test
-    void getTitle_ShouldBeEqual() {
+    public void getters_ShouldReturnCorrectData() {
         assertEquals("New question", question.getTitle());
-    }
-
-    @Test
-    void getBody_ShouldBeEqual() {
         assertEquals("Answer of question is", question.getBody());
-    }
-
-    @Test
-    public void getCreatedAt_ShouldReturnCreatedAt() {
         assertEquals(new Date().toString(), question.getCreatedAt().toString());
-    }
-
-    @Test
-    public void getUpdatedAt_ShouldReturnUpdatedAt() {
         assertEquals(new Date().toString(), question.getUpdatedAt().toString());
     }
 
     @Test
     public void equals_ShouldBeEqual() {
-        assertEquals(question, new Question("New question", "Body", 1L));
+        assertEquals(question, new Question("New question", "Body", 1L, 1L));
     }
 
     @Test
     public void equals_ShouldNotBeEqual() {
-        assertNotEquals(question, new Question("Title", "body", 1L));
+        assertNotEquals(question, new Question("Title", "body", 1L, 1L));
     }
 
     @Test
@@ -63,16 +51,17 @@ class QuestionTest {
 
     @Test
     public void hashcode_Symmetric_ShouldBeEqual() {
-        Question newQuestion = new Question("New question", "Body", 1L);
+        Question newQuestion = new Question("New question", "Body", 1L, 1L);
         assertTrue(question.equals(newQuestion) && newQuestion.equals(question));
         assertEquals(question.hashCode(), newQuestion.hashCode());
     }
 
     @Test
     public void toString_ShouldBeEqual() {
-        String toStr = String.format("Question(id=%s, title=%s, body=%s, createdAt=%s, updatedAt=%s, userId=%s)",
-                question.getId(), question.getTitle(), question.getBody(),
-                question.getCreatedAt(), question.getUpdatedAt(), question.getUserId());
+        String toStr = String
+                .format("Question(id=%s, title=%s, body=%s, createdAt=%s, updatedAt=%s, sectionId=%s, userId=%s)",
+                        question.getId(), question.getTitle(), question.getBody(), question.getCreatedAt(),
+                        question.getUpdatedAt(), question.getSectionId(), question.getUserId());
 
         assertEquals(toStr, question.toString());
     }
