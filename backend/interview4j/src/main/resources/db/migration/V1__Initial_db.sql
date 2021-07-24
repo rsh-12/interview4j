964a726f748801b0809ae3d4f70d4d1bff3735e8
rsh-12 <grinvichforum12@mail.ru>
2021-07-24 16:12:05 +0500

    Update initial_db sql

--- backend/interview4j/src/main/resources/db/migration/V1__Initial_db.sql
+++ backend/interview4j/src/main/resources/db/migration/V1__Initial_db.sql
@@ -15,10 +15,39 @@ create table role
     name varchar(64) not null unique
 );
 
-
-create table usr_role
+create table user_role
 (
-    usr_id  bigint references usr (id) on update cascade on delete cascade,
+    id      bigint primary key,
+    user_id bigint references usr (id) on update cascade on delete cascade,
     role_id bigint references role (id) on update cascade on delete cascade,
-    constraint usr_role_pkey primary key (usr_id, role_id)
+    unique (user_id, role_id)
+);
+create table section
+(
+    id         bigserial primary key,
+    title      varchar not null unique,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
+    user_id    bigint,
+    foreign key (user_id) references usr (id) on delete set null on update cascade
+);
+
+create table question
+(
+    id         bigserial primary key,
+    title      varchar not null unique,
+    body       text    not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
+    user_id    bigint references usr (id) on delete set null on update cascade,
+    section_id bigint references section (id) on delete set null on update cascade
+);
+
+create table question_rating
+(
+    id               bigserial primary key,
+    positive_answers int default 0,
+    negative_answers int default 0,
+    user_id          bigint references usr (id) on delete cascade on update cascade,
+    question_id      bigint references question (id) on delete cascade on update cascade
 );
