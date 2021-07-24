9f79c79ba69fcdae60493404f245724155d9f771
rsh-12 <grinvichforum12@mail.ru>
2021-07-24 21:38:39 +0500

    Add QuestionRating class

---
+++ backend/interview4j/src/main/java/ru/interview4j/domain/QuestionRating.java
@@ -0,0 +1,31 @@
+package ru.interview4j.domain;
+/*
+ * Date: 24.07.2021
+ * Time: 9:18 PM
+ * */
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@NoArgsConstructor
+@Table("question_rating")
+public class QuestionRating {
+
+    private Long id;
+    private int positiveAnswers;
+    private int negativeAnswers;
+    private Long userId;
+    private Long questionId;
+
+    @Builder(setterPrefix = "set")
+    private QuestionRating(int positiveAnswers, int negativeAnswers, Long userId, Long questionId) {
+        this.positiveAnswers = positiveAnswers;
+        this.negativeAnswers = negativeAnswers;
+        this.userId = userId;
+        this.questionId = questionId;
+    }
+}
+
