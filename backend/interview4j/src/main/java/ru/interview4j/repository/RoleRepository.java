b720ff7b7f2ed47fe6a4eef5dd4cbcd2f3fa9c3d
rsh-12 <grinvichforum12@mail.ru>
2021-07-24 23:58:03 +0500

    Add RoleRepository and query method

--- backend/interview4j/src/main/java/ru/interview4j/repository/RoleRepository.java
+++ backend/interview4j/src/main/java/ru/interview4j/repository/RoleRepository.java
@@ -1,7 +1,12 @@
 package ru.interview4j.repository;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 import ru.interview4j.domain.Role;
 
 public interface RoleRepository extends ReactiveCrudRepository<Role, Long> {
+
+    @Query("select name from role join usr_role ur on role.id = ur.role_id where usr_id = :userId")
+    Flux<Role> findUserRoles(Long userId);
 }
