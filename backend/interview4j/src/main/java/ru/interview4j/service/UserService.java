2e1dc4301f459be326b3addce451d30b8fd88bdf
rsh-12 <grinvichforum12@mail.ru>
2021-07-24 23:40:34 +0500

    Add UserService

---
+++ backend/interview4j/src/main/java/ru/interview4j/service/UserService.java
@@ -0,0 +1,7 @@
+package ru.interview4j.service;
+
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+
+public interface UserService extends ReactiveUserDetailsService {
+
+}
