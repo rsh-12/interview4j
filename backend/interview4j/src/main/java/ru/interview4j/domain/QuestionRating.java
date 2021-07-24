b7d9c2c60c2f81d655dddeef537e8965ee6c3924
rsh-12 <grinvichforum12@mail.ru>
2021-07-24 22:59:08 +0500

    Add Id and NotNull annotations

--- backend/interview4j/src/main/java/ru/interview4j/domain/QuestionRating.java
+++ backend/interview4j/src/main/java/ru/interview4j/domain/QuestionRating.java
@@ -7,17 +7,27 @@ package ru.interview4j.domain;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
 @Table("question_rating")
 public class QuestionRating {
 
+    @Id
     private Long id;
+
     private int positiveAnswers;
+
     private int negativeAnswers;
+
+    @NotNull
     private Long userId;
+
+    @NotNull
     private Long questionId;
 
     @Builder(setterPrefix = "set")
