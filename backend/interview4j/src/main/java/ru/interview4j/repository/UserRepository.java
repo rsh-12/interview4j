3e6c0b5bae3d0b8a435ccf4a13a937300c111fe2
rsh-12 <grinvichforum12@mail.ru>
2021-07-24 23:39:31 +0500

    Add findByUsername query method

--- backend/interview4j/src/main/java/ru/interview4j/repository/UserRepository.java
+++ backend/interview4j/src/main/java/ru/interview4j/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package ru.interview4j.repository;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 import ru.interview4j.domain.User;
 
 public interface UserRepository extends ReactiveCrudRepository<User, Long> {
+
+    Mono<User> findByUsername(String username);
 
 }
