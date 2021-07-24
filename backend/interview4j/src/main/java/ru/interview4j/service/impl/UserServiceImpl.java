8ac66384fcd79ef592f5be726375927ba603f837
rsh-12 <grinvichforum12@mail.ru>
2021-07-24 23:58:30 +0500

    Implement UserService

---
+++ backend/interview4j/src/main/java/ru/interview4j/service/impl/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package ru.interview4j.service.impl;
+/*
+ * Date: 24.07.2021
+ * Time: 11:22 PM
+ * */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import ru.interview4j.repository.UserRepository;
+import ru.interview4j.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Mono<UserDetails> findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .cast(UserDetails.class);
+    }
+
+}
