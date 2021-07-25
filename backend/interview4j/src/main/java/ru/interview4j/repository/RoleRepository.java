af10087065e6abc6828d31a465d4e985689303c1
rsh-12 <grinvichforum12@mail.ru>
2021-07-25 15:52:10 +0500

    Update query method

--- backend/interview4j/src/main/java/ru/interview4j/repository/RoleRepository.java
+++ backend/interview4j/src/main/java/ru/interview4j/repository/RoleRepository.java
@@ -1,6 +1,7 @@
 package ru.interview4j.repository;
 
 import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 import ru.interview4j.domain.Role;
@@ -8,5 +9,6 @@ import ru.interview4j.domain.Role;
 public interface RoleRepository extends ReactiveCrudRepository<Role, Long> {
 
     @Query("select id, name from role join user_role ur on role.id = ur.role_id where user_id = :userId")
-    Flux<Role> findUserRoles(Long userId);
+    Flux<Role> findUserRoles(@Param("userId") Long id);
+
 }
