ab3fa3f4be0d76bfc96651ef88b72e86818c0387
rsh-12 <grinvichforum12@mail.ru>
2021-07-29 13:12:25 +0500

    Add ProjectConfig class, bean modelmapper

---
+++ backend/interview4j/src/main/java/ru/interview4j/config/ProjectConfig.java
@@ -0,0 +1,21 @@
+package ru.interview4j.config;
+/*
+ * Date: 29.07.2021
+ * Time: 12:27 PM
+ * */
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setSkipNullEnabled(true);
+        return modelMapper;
+    }
+}
