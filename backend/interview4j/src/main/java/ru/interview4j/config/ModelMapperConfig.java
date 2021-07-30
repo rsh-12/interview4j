2200fcb53eb2b6c4212119b89945376bfcb656ca
rsh-12 <grinvichforum12@mail.ru>
2021-07-30 09:55:09 +0500

    Rename ProjectConfig to ModelMapperConfig

--- backend/interview4j/src/main/java/ru/interview4j/config/ModelMapperConfig.java
+++ backend/interview4j/src/main/java/ru/interview4j/config/ModelMapperConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ProjectConfig {
+public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
