0d4c26ab90cf85690475cd4995053d9842b08286
rsh-12 <grinvichforum12@mail.ru>
2021-08-01 00:30:03 +0500

    Add findUserRolesById test method

--- backend/interview4j/src/test/java/ru/interview4j/service/RoleServiceTest.java
+++ backend/interview4j/src/test/java/ru/interview4j/service/RoleServiceTest.java
@@ -11,12 +11,14 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import ru.interview4j.domain.ERole;
 import ru.interview4j.domain.Role;
 import ru.interview4j.repository.RoleRepository;
 import ru.interview4j.service.impl.RoleServiceImpl;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RoleServiceTest {
@@ -28,9 +30,27 @@ public class RoleServiceTest {
     private RoleRepository roleRepository;
 
     @Test
-    public void findUserRoles_ShouldReturnFluxEmpty() {
+    public void findUserRolesById_ShouldReturnFluxEmpty() {
         given(roleRepository.findRolesByUserId(anyLong())).willReturn(Flux.empty());
         StepVerifier.create(roleService.findUserRolesById(1L))
+                .verifyComplete();
+    }
+
+    @Test
+    public void findUserRolesById_ShouldReturnRoles() {
+        Role role_user = mock(Role.class);
+        given(role_user.getId()).willReturn(1L);
+        given(role_user.getName()).willReturn(ERole.ROLE_USER);
+
+        Role role_admin = mock(Role.class);
+        given(role_user.getId()).willReturn(2L);
+        given(role_user.getName()).willReturn(ERole.ROLE_ADMIN);
+
+        given(roleRepository.findRolesByUserId(anyLong())).willReturn(Flux.just(role_user, role_admin));
+
+        StepVerifier.create(roleService.findUserRolesById(1L))
+                .expectNext(role_user)
+                .expectNext(role_admin)
                 .verifyComplete();
     }
 
