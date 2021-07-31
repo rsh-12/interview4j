26d629e5bb6671c5216d5cef540197a9c2b7319d
rsh-12 <grinvichforum12@mail.ru>
2021-07-31 19:18:01 +0500

    Remove GrantedAuthority interface

--- backend/interview4j/src/main/java/ru/interview4j/domain/Role.java
+++ backend/interview4j/src/main/java/ru/interview4j/domain/Role.java
@@ -4,30 +4,23 @@ package ru.interview4j.domain;
  * Time: 10:46 AM
  * */
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
-import org.springframework.security.core.GrantedAuthority;
 
 @Data
 @NoArgsConstructor
 @RequiredArgsConstructor
 @Table("role")
-public class Role implements GrantedAuthority {
+public class Role {
 
     @Id
     private Long id;
 
-    @JsonIgnore
     @NonNull
     private ERole name;
 
-    @Override
-    public String getAuthority() {
-        return name.name();
-    }
 }
