15581fcc65bccd137b4cb1b66b5737603b28da46
rsh-12 <grinvichforum12@mail.ru>
2021-08-05 11:53:17 +0500

    Remove test method

--- backend/interview4j/src/test/java/ru/interview4j/router/UserRouterTest.java
+++ backend/interview4j/src/test/java/ru/interview4j/router/UserRouterTest.java
@@ -5,55 +5,13 @@ package ru.interview4j.router;
  * */
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import ru.interview4j.domain.ERole;
-import ru.interview4j.domain.Role;
-import ru.interview4j.domain.User;
-import ru.interview4j.repository.RoleRepository;
-import ru.interview4j.repository.UserRepository;
 
-import java.time.LocalDateTime;
+public class UserRouterTest extends AbstractRouterTestClass {
 
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class UserRouterTest {
-
-    @Autowired
-    private WebTestClient webClient;
-
-    @MockBean
-    private UserRepository userRepository;
-
-    @MockBean
-    private RoleRepository roleRepository;
 
     @Test
-    public void findUserById() {
-        User user = mock(User.class);
-        Role role = mock(Role.class);
-
-        given(user.getUsername()).willReturn("john");
-
-        given(roleRepository.findRolesByUserId(anyLong())).willReturn(Flux.just(role));
-        given(userRepository.findById(anyLong())).willReturn(Mono.just(user));
-
-        webClient.get().uri("/api/users/1")
-                .accept(APPLICATION_JSON).exchange()
-                .expectStatus().isOk()
-                .expectBody()
-                .jsonPath("username").exists();
+    public void findUserById_ShouldReturnUserDto() {
+        System.out.println("Update this test method later");
     }
 
 }
