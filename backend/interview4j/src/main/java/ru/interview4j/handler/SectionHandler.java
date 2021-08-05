5780425d77b66d673f779f2f9760256d8ef679ed
rsh-12 <grinvichforum12@mail.ru>
2021-08-05 10:15:28 +0500

    Add SectionHandler class

---
+++ backend/interview4j/src/main/java/ru/interview4j/handler/SectionHandler.java
@@ -0,0 +1,12 @@
+package ru.interview4j.handler;
+/*
+ * Date: 05.08.2021
+ * Time: 10:14 AM
+ * */
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SectionHandler {
+
+}
