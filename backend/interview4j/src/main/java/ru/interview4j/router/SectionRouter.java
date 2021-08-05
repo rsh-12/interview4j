65112b85d0312e82e612d4a4d2c27bf79c093b97
rsh-12 <grinvichforum12@mail.ru>
2021-08-05 10:15:22 +0500

    Add SectionRouter class

---
+++ backend/interview4j/src/main/java/ru/interview4j/router/SectionRouter.java
@@ -0,0 +1,19 @@
+package ru.interview4j.router;
+/*
+ * Date: 05.08.2021
+ * Time: 10:12 AM
+ * */
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SectionRouter {
+
+    // get one
+    // get all
+    // create
+    // delete
+    // update
+
+
+}
