7711f2efcd117ca10d4d47c0239a559b9edd5382
rsh-12 <grinvichforum12@mail.ru>
2021-08-05 12:12:38 +0500

    Add findUserById test method (ok)

--- backend/interview4j/src/test/java/ru/interview4j/router/UserRouterTest.java
+++ backend/interview4j/src/test/java/ru/interview4j/router/UserRouterTest.java
@@ -5,13 +5,38 @@ package ru.interview4j.router;
  * */
 
 import org.junit.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import reactor.core.publisher.Mono;
+import ru.interview4j.domain.User;
+import ru.interview4j.dto.UserDto;
+import ru.interview4j.service.UserService;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 public class UserRouterTest extends AbstractRouterTestClass {
 
+    private static final String API_USERS = "/api/users";
+
+    @MockBean
+    private UserService userService;
 
     @Test
     public void findUserById_ShouldReturnUserDto() {
-        System.out.println("Update this test method later");
+        User user = mock(User.class);
+        given(userService.findUserById(anyLong())).willReturn(Mono.just(user));
+        given(userService.mapToUserDto(user)).willReturn(new UserDto(USERNAME, CREATED_AT_NOW, UPDATED_AT_NOW));
+
+        webClient.get().uri(API_USERS + "/1")
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("username").exists()
+                .jsonPath("createdAt").exists()
+                .jsonPath("updatedAt").exists();
     }
 
 }
