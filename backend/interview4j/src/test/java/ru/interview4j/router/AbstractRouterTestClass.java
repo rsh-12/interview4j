6da945c071184c4307025ef5dec440ffb10998c6
rsh-12 <grinvichforum12@mail.ru>
2021-08-05 12:11:24 +0500

    Add some helper fields

--- backend/interview4j/src/test/java/ru/interview4j/router/AbstractRouterTestClass.java
+++ backend/interview4j/src/test/java/ru/interview4j/router/AbstractRouterTestClass.java
@@ -10,9 +10,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.time.LocalDateTime;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 abstract class AbstractRouterTestClass {
+
+    protected static final LocalDateTime CREATED_AT_NOW = LocalDateTime.now();
+    protected static final LocalDateTime UPDATED_AT_NOW = LocalDateTime.now();
+    protected static final String USERNAME = "username";
+    protected static final String PASSWORD = "password12345";
 
     @Autowired
     protected WebTestClient webClient;
