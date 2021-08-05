48c40786b893fe409c481c148ad05a525eb4aa8b
rsh-12 <grinvichforum12@mail.ru>
2021-08-05 10:24:59 +0500

    Add getSectionById stub method

--- backend/interview4j/src/main/java/ru/interview4j/handler/SectionHandler.java
+++ backend/interview4j/src/main/java/ru/interview4j/handler/SectionHandler.java
@@ -4,9 +4,17 @@ package ru.interview4j.handler;
  * Time: 10:14 AM
  * */
 
+import lombok.NonNull;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 @Component
 public class SectionHandler {
+
+    public @NonNull Mono<ServerResponse> getSectionById(ServerRequest request) {
+        return null;
+    }
 
 }
