45076c03f47aed672285bf76edda702421dfa4a1
rsh-12 <grinvichforum12@mail.ru>
2021-08-05 11:52:58 +0500

    Update getSectionById method

--- backend/interview4j/src/main/java/ru/interview4j/handler/SectionHandler.java
+++ backend/interview4j/src/main/java/ru/interview4j/handler/SectionHandler.java
@@ -5,16 +5,37 @@ package ru.interview4j.handler;
  * */
 
 import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+import ru.interview4j.domain.Section;
+import ru.interview4j.exception.CustomException;
+import ru.interview4j.service.SectionService;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Component
 public class SectionHandler {
 
+    private final SectionService sectionService;
+
+    @Autowired
+    public SectionHandler(SectionService sectionService) {
+        this.sectionService = sectionService;
+    }
+
     public @NonNull Mono<ServerResponse> getSectionById(ServerRequest request) {
-        return null;
+        Long sectionId = Long.valueOf(request.pathVariable("id"));
+        Mono<Section> section = sectionService.findSectionById(sectionId)
+                .switchIfEmpty(Mono.error(() -> CustomException.notFound("Section not found")));
+
+        return section.map(sectionService::mapToSectionDto)
+                .flatMap(sectionDto -> ok().contentType(APPLICATION_JSON)
+                        .body(fromValue(sectionDto)));
     }
 
 }
