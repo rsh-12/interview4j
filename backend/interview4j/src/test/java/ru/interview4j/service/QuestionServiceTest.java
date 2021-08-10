01389734b19ff7fce2a5268e6b2de724291ad471
Ivanshelemeh <neo041990@mail.ru>
2021-08-10 17:17:25 +0300

    Test oh god save is's been passed

--- backend/interview4j/src/test/java/ru/interview4j/service/QuestionServiceTest.java
+++ backend/interview4j/src/test/java/ru/interview4j/service/QuestionServiceTest.java
@@ -1,44 +1,52 @@
 package ru.interview4j.service;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 import ru.interview4j.domain.Question;
 import ru.interview4j.repository.QuestionRepository;
 import ru.interview4j.service.impl.QuestionServiceImpl;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
 public class QuestionServiceTest {
 
     @InjectMocks
   private QuestionServiceImpl questionService;
 
-    @MockBean
+    @Mock
    private QuestionRepository repo;
 
-   @Before
-  public void setUp(){
-       Question question = Question.builder().setTitle("Freedom").setBody("Phill Morris").setSectionId(2L).setUserId(4L).build();
-      this.repo.save(question);
-   }
+    @Mock
+   private QuestionServ questionServ;
+
 
 
     @Test
     @DisplayName("test for finding  by title")
  public   void testFoundQuestionByTitle(){
+        Question question = Question.builder().setTitle("Freedom").setBody("Daddy").setUserId(1L).setSectionId(4L).build();
+        given(repo.findBySpecificTitle(anyString())).willReturn(Mono.just(question));
         StepVerifier.create(this.questionService.getTitle("Freedom"))
                 .expectSubscription()
-               // .expectNextMatches(question -> question.getTitle().equals("Freedom"))
+               .expectNextMatches(q-> question.getTitle().equals("Freedom"))
                 .verifyComplete();
 
 
 
     }
+
+
+
 
 
 }
