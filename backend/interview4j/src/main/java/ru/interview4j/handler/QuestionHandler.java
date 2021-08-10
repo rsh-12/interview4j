1a722f362154567f9167b1a0fa659785079d76c2
rsh-12 <grinvichforum12@mail.ru>
2021-08-10 07:10:11 +0500

    Add getQuestionById method

--- backend/interview4j/src/main/java/ru/interview4j/handler/QuestionHandler.java
+++ backend/interview4j/src/main/java/ru/interview4j/handler/QuestionHandler.java
@@ -5,16 +5,37 @@ package ru.interview4j.handler;
  * */
 
 import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+import ru.interview4j.dto.QuestionDto;
+import ru.interview4j.service.QuestionService;
+import ru.interview4j.util.CustomMapper;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Component
 public class QuestionHandler {
 
+    private final QuestionService questionService;
+
+    @Autowired
+    public QuestionHandler(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
     public @NonNull Mono<ServerResponse> getQuestionById(ServerRequest request) {
-        return null;
+        Long questionId = Long.valueOf(request.pathVariable("id"));
+        Mono<QuestionDto> question = questionService.findQuestionById(questionId)
+                .map(CustomMapper::mapToDto);
+
+        return question.flatMap(questionDto -> ok()
+                .contentType(APPLICATION_JSON)
+                .body(fromValue(questionDto)));
     }
 
 }
