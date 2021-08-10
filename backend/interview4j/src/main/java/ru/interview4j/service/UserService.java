6f1346e9b9879e4d0b7ffd790407665b252ebaa2
rsh-12 <grinvichforum12@mail.ru>
2021-08-10 07:11:04 +0500

    Remove some methods

--- backend/interview4j/src/main/java/ru/interview4j/service/UserService.java
+++ backend/interview4j/src/main/java/ru/interview4j/service/UserService.java
@@ -3,7 +3,6 @@ package ru.interview4j.service;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import reactor.core.publisher.Mono;
 import ru.interview4j.domain.User;
-import ru.interview4j.dto.UserDto;
 import ru.interview4j.router.request.AuthRequest;
 
 public interface UserService extends ReactiveUserDetailsService {
@@ -12,7 +11,4 @@ public interface UserService extends ReactiveUserDetailsService {
 
     Mono<User> register(AuthRequest credentials);
 
-    Mono<Boolean> existsByUsername(String username);
-
-    UserDto mapToUserDto(User user);
 }
