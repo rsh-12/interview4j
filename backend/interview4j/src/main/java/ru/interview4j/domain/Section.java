13ecfdf4d01423367eda406268032d6e0642c286
rsh-12 <grinvichforum12@mail.ru>
2021-10-12 18:37:34 +0500

    Add some methods related to question entity

--- backend/interview4j/src/main/java/ru/interview4j/domain/Section.java
+++ backend/interview4j/src/main/java/ru/interview4j/domain/Section.java
@@ -12,9 +12,12 @@ import lombok.NonNull;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * This class is associated with the {@link User} and {@link Question} classes.
@@ -41,6 +44,9 @@ public class Section {
     @LastModifiedDate
     private LocalDateTime updatedAt;
 
+    @Transient
+    private Set<Question> questions = new HashSet<>();
+
     @NonNull
     private Long userId;
 
@@ -48,6 +54,35 @@ public class Section {
     public Section(@NonNull String title, @NonNull Long userId) {
         this.title = title;
         this.userId = userId;
+    }
+
+    public void setQuestions(Set<Question> questions) {
+        if (this.questions != null) {
+            this.questions.forEach(question -> question.setSection(null));
+        }
+
+        if (questions != null) {
+            questions.forEach(question -> question.setSection(this));
+        }
+
+        this.questions = questions;
+    }
+
+    public Section addQuestion(Question question) {
+        this.questions.add(question);
+        question.setSection(this);
+        return this;
+    }
+
+    public Section removeQuestion(Question question) {
+        this.questions.remove(question);
+        question.setSection(null);
+        return this;
+    }
+
+    public Section quetions(Set<Question> questions) {
+        this.setQuestions(questions);
+        return this;
     }
 
 }
