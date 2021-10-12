f94dd1b07f7d251e337f1b74081c65811866be40
rsh-12 <grinvichforum12@mail.ru>
2021-10-12 18:41:10 +0500

    Add save method, transactional annotation

--- backend/interview4j/src/main/java/ru/interview4j/service/impl/SectionServiceImpl.java
+++ backend/interview4j/src/main/java/ru/interview4j/service/impl/SectionServiceImpl.java
@@ -4,8 +4,10 @@ package ru.interview4j.service.impl;
  * Time: 11:30 AM
  * */
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.interview4j.domain.Section;
@@ -15,7 +17,9 @@ import ru.interview4j.service.SectionService;
 
 import java.util.Comparator;
 
+@Slf4j
 @Service
+@Transactional
 public class SectionServiceImpl implements SectionService {
 
     private final SectionRepository sectionRepository;
@@ -37,6 +41,12 @@ public class SectionServiceImpl implements SectionService {
                 .skip(page * size)
                 .take(size)
                 .switchIfEmpty(Mono.error(() -> CustomException.notFound("No sections found")));
+    }
+
+    @Override
+    public Mono<Section> save(Section section) {
+        log.debug("Request to save Section: {}", section);
+        return sectionRepository.save(section);
     }
 
 }
